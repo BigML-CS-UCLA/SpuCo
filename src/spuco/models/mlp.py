63acd2797e1852ae93d6b25445f0792fadc165a8
Siddharth Joshi <sjoshi804@gmail.com>
2023-05-14 12:45:06 -0700

    mlp changed

--- src/spuco/models/mlp.py
+++ src/spuco/models/mlp.py
@@ -1,20 +1,25 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-# Acknowledgement to
-# https://github.com/kuangliu/pytorch-cifar,
-# https://github.com/BIGBALLON/CIFAR-ZOO,
-
-''' MLP '''
+ 
 class MLP(nn.Module):
-    def __init__(self, channel):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int,
+    ):
         super(MLP, self).__init__()
-        self.fc_1 = nn.Linear(28*28*1 if channel==1 else 32*32*3, 128)
-        self.fc_2 = nn.Linear(128, 128)
-        self.representation_dim = 128
-
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.fc1 = nn.Linear(input_dim, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, output_dim)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=0.5)
+        
     def forward(self, x):
-        out = x.view(x.size(0), -1)
-        out = F.relu(self.fc_1(out))
-        out = F.relu(self.fc_2(out))
-        return out
+        x = x.view(-1, self.input_dim)
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
