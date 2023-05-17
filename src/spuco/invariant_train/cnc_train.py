5c4bf428d29238ca5b80a90a6e92b1111a03fac6
Siddharth Joshi <sjoshi804@gmail.com>
2023-05-17 14:48:55 -0700

    cnc comments added

--- src/spuco/invariant_train/cnc_train.py
+++ src/spuco/invariant_train/cnc_train.py
@@ -33,7 +33,9 @@ class CorrectNContrastTrain(BaseInvariantTrain):
             outputs = self.model(inputs)
             loss = lambda_ce * self.criterion(outputs, labels)
 
+            # Contrastive Loss
             for anchor_idx in range(len(batch)):
+                # Only computed with majority examples as anchor
                 if labels[anchor_idx] != groups[anchor_idx]:
                     continue 
                 pos_idx = []
@@ -53,8 +55,8 @@ class CorrectNContrastTrain(BaseInvariantTrain):
                     pos_sim = torch.exp(torch.cosine_similarity(anchor, pos))
                     pos_sum_sim = torch.sum(pos_sim) 
                     neg_sum_sim = torch.sum(torch.exp(torch.cosine_similarity(anchor, neg)))
-                    sup_cl_loss = torch.sum(torch.log(pos_sim / ((pos_sum_sim + neg_sum_sim) / (num_pos + num_neg))))
-                    loss += sup_cl_loss
+                    sup_cl_loss = torch.sum(torch.log(pos_sim / (pos_sum_sim + neg_sum_sim)))
+                    loss += sup_cl_loss / len(pos_idx)
 
             return loss, outputs, labels
 
