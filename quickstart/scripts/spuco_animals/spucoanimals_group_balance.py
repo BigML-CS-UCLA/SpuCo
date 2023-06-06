56782480dd32c8dee8da608237b9f0f227e9c52a
YuYang0901 <48270938+YuYang0901@users.noreply.github.com>
2023-06-06 01:13:09 -0700

    SpuCoAnimals Group Balancing script

---
+++ quickstart/scripts/spuco_animals/spucoanimals_group_balance.py
@@ -0,0 +1,141 @@
+import argparse
+import os
+
+import pandas as pd
+import torch
+import torchvision.transforms as transforms
+from torch.optim import SGD
+
+from spuco.evaluate import Evaluator
+from spuco.invariant_train import GroupBalanceBatchERM
+from spuco.models import model_factory
+from spuco.utils import set_seed
+from spuco.datasets import SpuCoAnimals
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gpu", type=int, default=0)
+parser.add_argument("--seed", type=int, default=0)
+parser.add_argument("--root_dir", type=str, default="/data")
+parser.add_argument("--label_noise", type=float, default=0.0)
+parser.add_argument("--results_csv", type=str, default="results/spucoanimals_group_balance.csv")
+
+parser.add_argument("--arch", type=str, default="resnet18")
+parser.add_argument("--batch_size", type=int, default=128)
+parser.add_argument("--num_epochs", type=int, default=100)
+parser.add_argument("--lr", type=float, default=1e-4)
+parser.add_argument("--weight_decay", type=float, default=1e-4)
+parser.add_argument("--momentum", type=float, default=0.9)
+parser.add_argument("--pretrained", action="store_true")
+
+args = parser.parse_args()
+
+device = torch.device(f"cuda:{args.gpu}" if torch.cuda.is_available() else "cpu")
+set_seed(args.seed)
+
+# Load the full dataset, and download it if necessary
+transform = transforms.Compose([
+            transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
+        ])
+
+trainset = SpuCoAnimals(
+    root=args.root_dir,
+    label_noise=args.label_noise,
+    split="train",
+    transform=transform,
+)
+trainset.initialize()
+
+valset = SpuCoAnimals(
+    root=args.root_dir,
+    label_noise=args.label_noise,
+    split="val",
+    transform=transform,
+)
+valset.initialize()
+
+testset = SpuCoAnimals(
+    root=args.root_dir,
+    label_noise=args.label_noise,
+    split="test",
+    transform=transform,
+)
+testset.initialize()
+
+model = model_factory(args.arch, trainset[0][0].shape, trainset.num_classes, pretrained=args.pretrained).to(device)
+
+valid_evaluator = Evaluator(
+    testset=valset,
+    group_partition=valset.group_partition,
+    group_weights=valset.group_weights,
+    batch_size=args.batch_size,
+    model=model,
+    device=device,
+    verbose=True
+)
+
+group_balance = GroupBalanceBatchERM(
+    model=model,
+    group_partition=trainset.group_partition,
+    val_evaluator=valid_evaluator,
+    num_epochs=args.num_epochs,
+    trainset=trainset,
+    batch_size=args.batch_size,
+    optimizer=SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum),
+    device=device,
+    verbose=True
+)
+group_balance.train()
+
+evaluator = Evaluator(
+    testset=testset,
+    group_partition=testset.group_partition,
+    group_weights=trainset.group_weights,
+    batch_size=args.batch_size,
+    model=model,
+    device=device,
+    verbose=True
+)
+evaluator.evaluate()
+
+results = pd.DataFrame(index=[0])
+results["timestamp"] = pd.Timestamp.now()
+results["seed"] = args.seed
+results["pretrained"] = args.pretrained
+results["arch"] = args.arch
+results["lr"] = args.lr
+results["weight_decay"] = args.weight_decay
+results["momentum"] = args.momentum
+results["num_epochs"] = args.num_epochs
+results["batch_size"] = args.batch_size
+
+results["worst_group_accuracy"] = evaluator.worst_group_accuracy[1]
+results["average_accuracy"] = evaluator.average_accuracy
+
+evaluator = Evaluator(
+    testset=testset,
+    group_partition=testset.group_partition,
+    group_weights=trainset.group_weights,
+    batch_size=args.batch_size,
+    model=group_balance.best_model,
+    device=device,
+    verbose=True
+)
+evaluator.evaluate()
+
+results["early_stopping_worst_group_accuracy"] = evaluator.worst_group_accuracy[1]
+results["early_stopping_average_accuracy"] = evaluator.average_accuracy
+
+print(results)
+
+if os.path.exists(args.results_csv):
+    results_df = pd.read_csv(args.results_csv)
+else:
+    results_df = pd.DataFrame()
+
+results_df = pd.concat([results_df, results], ignore_index=True)
+results_df.to_csv(args.results_csv, index=False)
+
+print('Done!')
+print('Results saved to', args.results_csv)
+
+
