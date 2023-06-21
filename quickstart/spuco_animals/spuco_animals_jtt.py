d3e3da2aa38e24efa459184c277407a07d886ab1
Siddharth Joshi <sjoshi804@gmail.com>
2023-06-21 03:42:04 -0700

    reorganized nbs and scripts

--- quickstart/spuco_animals/spuco_animals_jtt.py
+++ quickstart/spuco_animals/spuco_animals_jtt.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import torch
 import torchvision.transforms as transforms
-from sklearn.metrics import precision_score, precision_score
+from sklearn.metrics import f1_score
 from torch.optim import SGD
 from wilds import get_dataset
 
@@ -27,12 +27,12 @@ parser.add_argument("--results_csv", type=str, default="results/spucoanimals_jtt
 parser.add_argument("--arch", type=str, default="resnet18")
 parser.add_argument("--batch_size", type=int, default=128)
 parser.add_argument("--num_epochs", type=int, default=100)
-parser.add_argument("--lr", type=float, default=1e-4)
+parser.add_argument("--lr", type=float, default=1e-3)
 parser.add_argument("--weight_decay", type=float, default=1e-4)
 parser.add_argument("--momentum", type=float, default=0.9)
 parser.add_argument("--pretrained", action="store_true")
 
-parser.add_argument("--infer_num_epochs", type=int, default=7)
+parser.add_argument("--infer_num_epochs", type=int, default=10)
 
 parser.add_argument("--upsample_factor", type=int, default=100)
 
@@ -70,32 +70,120 @@ testset = SpuCoAnimals(
 )
 testset.initialize()
 
+model = model_factory(args.arch, trainset[0][0].shape, trainset.num_classes, pretrained=args.pretrained).to(device)
 
-max_precision = 0
-logits_files = glob(f"logits/spucoanimals/lr=0.001_wd=0.0001_seed={args.seed}/valset*.pt")
-for logits_file in logits_files:
-    epoch = int(logits_file.split("/")[-1].split(".")[0].split("_")[-1])
-    if epoch >= args.num_epochs:
-        continue
-    logits = torch.load(logits_file)
-    predictions = torch.argmax(logits, dim=-1).detach().cpu().tolist()
-    jtt = JTTInference(
-        predictions=predictions,
-        class_labels=valset.labels
-    )
-    epoch_group_partition = jtt.infer_groups()
+trainer = Trainer(
+    trainset=trainset,
+    model=model,
+    batch_size=args.batch_size,
+    optimizer=SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum),
+    device=device,
+    verbose=True
+)
+trainer.train(num_epochs=args.infer_num_epochs)
 
-    upsampled_indices = epoch_group_partition[(0,1)]
-    minority_indices = valset.group_partition[(0,1)]
-    minority_indices.extend(valset.group_partition[(1,0)])
-    # compute precision score on the validation set
-    upsampled = np.zeros(len(predictions))
-    upsampled[np.array(upsampled_indices)] = 1
-    minority = np.zeros(len(predictions))
-    minority[np.array(minority_indices)] = 1
-    precision = precision_score(minority, upsampled)
-    if precision > max_precision:
-        max_precision = precision
-        args.infer_num_epochs = epoch
-        group_partition = epoch_group_partition
-        print("New best precision score:", precision, "at epoch", epoch)
+predictions = torch.argmax(trainer.get_trainset_outputs(), dim=-1).detach().cpu().tolist()
+jtt = JTTInference(
+    predictions=predictions,
+    class_labels=trainset.labels
+)
+
+group_partition = jtt.infer_groups()
+
+for key in sorted(group_partition.keys()):
+    print(key, len(group_partition[key]))
+evaluator = Evaluator(
+    testset=trainset,
+    group_partition=group_partition,
+    group_weights=trainset.group_weights,
+    batch_size=args.batch_size,
+    model=model,
+    device=device,
+    verbose=True
+)
+evaluator.evaluate()
+
+robust_trainset = GroupLabeledDatasetWrapper(trainset, group_partition)
+
+val_evaluator = Evaluator(
+    testset=testset,
+    group_partition=testset.group_partition,
+    group_weights=trainset.group_weights,
+    batch_size=args.batch_size,
+    model=model,
+    device=device,
+    verbose=True
+)
+
+indices = []
+indices.extend(group_partition[(0,0)])
+indices.extend(group_partition[(0,1)] * args.upsample_factor)
+
+print("Training on", len(indices), "samples")
+
+model = model_factory(args.arch, trainset[0][0].shape, trainset.num_classes, pretrained=args.pretrained).to(device)
+jtt_train = CustomSampleERM(
+    model=model,
+    num_epochs=args.num_epochs,
+    trainset=trainset,
+    batch_size=args.batch_size,
+    indices=indices,
+    val_evaluator=val_evaluator,
+    optimizer=SGD(model.parameters(), lr=args.lr, weight_decay=args.weight_decay, momentum=args.momentum),
+    device=device,
+    verbose=True
+)
+jtt_train.train()
+
+evaluator = Evaluator(
+    testset=testset,
+    group_partition=testset.group_partition,
+    group_weights=trainset.group_weights,
+    batch_size=args.batch_size,
+    model=model,
+    device=device,
+    verbose=True
+)
+evaluator.evaluate()
+results = pd.DataFrame(index=[0])
+results["timestamp"] = pd.Timestamp.now()
+results["seed"] = args.seed
+results["pretrained"] = args.pretrained
+results["lr"] = args.lr
+results["weight_decay"] = args.weight_decay
+results["momentum"] = args.momentum
+results["num_epochs"] = args.num_epochs
+results["batch_size"] = args.batch_size
+results["upsample_factor"] = args.upsample_factor
+results["infer_num_epochs"] = args.infer_num_epochs
+
+results["worst_group_accuracy"] = evaluator.worst_group_accuracy[1]
+results["average_accuracy"] = evaluator.average_accuracy
+
+evaluator = Evaluator(
+    testset=testset,
+    group_partition=testset.group_partition,
+    group_weights=trainset.group_weights,
+    batch_size=args.batch_size,
+    model=jtt_train.best_model,
+    device=device,
+    verbose=True
+)
+evaluator.evaluate()
+
+
+results["early_stopping_worst_group_accuracy"] = evaluator.worst_group_accuracy[1]
+results["early_stopping_average_accuracy"] = evaluator.average_accuracy
+
+if os.path.exists(args.results_csv):
+    results_df = pd.read_csv(args.results_csv)
+else:
+    results_df = pd.DataFrame()
+
+results_df = pd.concat([results_df, results], ignore_index=True)
+results_df.to_csv(args.results_csv, index=False)
+
+print('Done!')
+print('Results saved to', args.results_csv)
+
+
